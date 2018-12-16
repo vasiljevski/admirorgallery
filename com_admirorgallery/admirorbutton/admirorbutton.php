ccd928cc79b28811ef03ad4cdf8208b63aecf31d
Nikola Vasiljevski <nikola83@gmail.com>
2018-12-16 09:29:35 -0800

    Template framework refactored
    - Code duplication  improved
    - Optimisations added
    - Version update to 5.2.0
    - Also year updated to 2018

--- com_admirorgallery/admirorbutton/admirorbutton.php
+++ com_admirorgallery/admirorbutton/admirorbutton.php
@@ -1,10 +1,10 @@
 <?php
 /**
- * @version     5.1.2
+ * @version     5.2.0
  * @package     Admiror Gallery (plugin)
  * @subpackage  admirorbutton
  * @author      Igor Kekeljevic & Nikola Vasiljevski
- * @copyright   Copyright (C) 2010 - 2017 http://www.admiror-design-studio.com All Rights Reserved.
+ * @copyright   Copyright (C) 2010 - 2018 http://www.admiror-design-studio.com All Rights Reserved.
  * @license     http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
 
