3fb1b8d6639936ac93d5ec527263c028c9817395
Nikola Vasiljevski <nikola83@gmail.com>
2020-12-29 02:13:52 +0100

    Updated version to 5.5.0

--- com_admirorgallery/admin/models/resourcemanager.php
+++ com_admirorgallery/admin/models/resourcemanager.php
@@ -3,7 +3,7 @@
  * @version     5.5.0
  * @package     Admiror Gallery (component)
  * @author      Igor Kekeljevic & Nikola Vasiljevski
- * @copyright   Copyright (C) 2010 - 2018 http://www.admiror-design-studio.com All Rights Reserved.
+ * @copyright   Copyright (C) 2010 - 2020 http://www.admiror-design-studio.com All Rights Reserved.
  * @license     http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
 
--- com_admirorgallery/admirorgallery/admirorgallery/core/agGallery.php
+++ com_admirorgallery/admirorgallery/admirorgallery/core/agGallery.php
@@ -4,7 +4,7 @@
  * @package     Admiror Gallery (plugin)
  * @subpackage  admirorgallery
  * @author      Igor Kekeljevic & Nikola Vasiljevski
- * @copyright   Copyright (C) 2010 - 2018 http://www.admiror-design-studio.com All Rights Reserved.
+ * @copyright   Copyright (C) 2010 - 2020 http://www.admiror-design-studio.com All Rights Reserved.
  * @license     http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
 
