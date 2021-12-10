007394fab7ead2d6b01ce21678b7f35e5f1d4159
Nikola Vasiljevski <nikola83@gmail.com>
2021-12-10 23:56:30 +0100

    Updated code to fit to J4

--- com_admirorgallery/admirorbutton/admirorbutton.php
+++ com_admirorgallery/admirorbutton/admirorbutton.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @version     6.0.0
  * @package     Admiror Gallery (plugin)
@@ -10,65 +11,95 @@
 
 defined('_JEXEC') or die();
 
+use Joomla\CMS\Factory;
+use Joomla\CMS\Language\Text;
+use Joomla\CMS\Object\CMSObject;
+use Joomla\CMS\Plugin\CMSPlugin;
+use Joomla\CMS\Uri\Uri as JUri;
+
 /**
  * Editor Image button
  *
  * @package Editors-xtd
  * @since 1.5
  */
-class plgButtonAdmirorbutton extends JPlugin {
+class plgButtonAdmirorbutton extends CMSPlugin
+{
+    /**
+     * Load the language file on instantiation.
+     *
+     * @var    boolean
+     * @since  3.1
+     */
     protected $autoloadLanguage = true;
 
     /**
-     * Constructor
+     * Display the button.
      *
-     * For php4 compatibility we must not use the __constructor as a constructor for plugins
-     * because func_get_args ( void ) returns a copy of all passed arguments NOT references.
-     * This causes problems with cross-referencing necessary for the observer design pattern.
+     * @param   string   $name    The name of the button to display.
+     * @param   string   $asset   The name of the asset being edited.
+     * @param   integer  $author  The id of the author owning the asset being edited.
      *
-     * @param 	object $subject The object to observe
-     * @param 	array  $config  An array that holds the plugin configuration
+     * @return  CMSObject|false
      *
-     * @since 1.5
+     * @since   5.5.0
      */
-    public function __construct(object &$subject, array $config) {
-        parent::__construct($subject, $config);
-        $this->loadLanguage('com_admirorgallery');
-        $this->loadLanguage('plg_editors-xtd_admirorbutton');   
-    }
-
-    /**
-     * Display the button
-     *
-     * @param string name
-     *
-     * @return JObject
-     *
-     * @since 5.5.0
-     */
-    function onDisplay(string $name): JObject
+    //function onDisplay(string $name): JObject
+    public function onDisplay($name, $asset, $author)
     {
-        $doc = JFactory::getDocument();
-        $doc->addStyleSheet(JURI::root() . 'administrator/components/com_admirorgallery/templates/default/css/add-trigger.css');
-        $doc->addScriptDeclaration("            
-		function insertTriggerCode(txt) {
-		    if(!txt) return;
-		    jInsertEditorText(txt, '" . $name . "');
-		}
-		");
+        $app       = Factory::getApplication();
+        $doc       = $app->getDocument();
+        $user      = $app->getIdentity();
+        $extension = $app->input->get('option');
 
-        $link = 'index.php?option=com_admirorgallery&amp;view=button&amp;tmpl=component&amp;e_name=' . $name;
+        // For categories we check the extension (ex: component.section)
+        if ($extension === 'com_categories') {
+            $parts     = explode('.', $app->input->get('extension', 'com_content'));
+            $extension = $parts[0];
+        }
 
-        JHTML::_('behavior.modal');
+        $asset = $asset !== '' ? $asset : $extension;
 
-        $button = new JObject();
-        $button->set('class','btn');
-        $button->set('modal', true); // modal dialog
-        $button->set('link', $link); //link to open on click
-        $button->set('text', JText::_('COM_ADMIRORGALLERY')); //button text
-        $button->set('name', 'admirorgallery'); //div class
-        $button->set('options', "{handler: 'iframe', size: {x: 400, y: 300}}"); //need to work
+        if (
+            $user->authorise('core.edit', $asset)
+            || $user->authorise('core.create', $asset)
+            || (count($user->getAuthorisedCategories($asset, 'core.create')) > 0)
+            || ($user->authorise('core.edit.own', $asset) && $author === $user->id)
+            || (count($user->getAuthorisedCategories($extension, 'core.edit')) > 0)
+            || (count($user->getAuthorisedCategories($extension, 'core.edit.own')) > 0 && $author === $user->id)
+        ) {
+            $doc->addStyleSheet(JUri::root() . 'administrator/components/com_admirorgallery/templates/default/css/add-trigger.css');
+            $doc->addScriptDeclaration("            
+            function insertTriggerCode(txt) {
+                if(!txt) return;
+                jInsertEditorText(txt, '" . $name . "');
+            }
+            ");
 
-        return $button;
+            $link = 'index.php?option=com_admirorgallery&amp;view=button&amp;tmpl=component&amp;e_name=' . $name;
+
+            $button = new CMSObject();
+            $button->modal   = true;
+            $button->link    = $link;
+            $button->text    = Text::_('COM_ADMIRORGALLERY');
+            $button->name    = $this->_type . '_' . $this->_name;
+            $button->icon    = 'pictures';
+            $button->iconSVG = '<svg width="24" height="24" viewBox="0 0 512 512"><path d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48'
+                . ' 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm-6 336H54a6 6 0 0 1-6-6V118a6 6 0 0 1 6-6h404a6 6'
+                . ' 0 0 1 6 6v276a6 6 0 0 1-6 6zM128 152c-22.091 0-40 17.909-40 40s17.909 40 40 40 40-17.909 40-40-17.909-40-40-40'
+                . 'zM96 352h320v-80l-87.515-87.515c-4.686-4.686-12.284-4.686-16.971 0L192 304l-39.515-39.515c-4.686-4.686-12.284-4'
+                . '.686-16.971 0L96 304v48z"></path></svg>';
+            $button->options = [
+                'height'          => '300px',
+                'width'           => '400px',
+                'bodyHeight'      => '70',
+                'modalWidth'      => '80',
+                'tinyPath'        => $link,
+                'handler'         => 'iframe',
+            ];
+
+            return $button;
+        }
+        return false;
     }
 }
