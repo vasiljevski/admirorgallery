466d49846b2e11c6e9a723c204e25d301dd1b4e5
Nikola Vasiljevski <nikola83@gmail.com>
2021-12-21 23:42:11 +0100

    Fixed site gallery manu item and optimized code

--- com_admirorgallery/site/views/layout/tmpl/default.php
+++ com_admirorgallery/site/views/layout/tmpl/default.php
@@ -9,47 +9,30 @@
 
 defined('_JEXEC') or die();
 
-$ag_inlineParams='';
-$ag_inlineParams.=' template="'.$this->template.'"';
-$ag_inlineParams.=' thumbWidth="'.$this->thumbWidth.'"';
-$ag_inlineParams.=' thumbHeight="'.$this->thumbHeight.'"';
-$ag_inlineParams.=' thumbAutoSize="'.$this->thumbAutoSize.'"';
-$ag_inlineParams.=' arrange="'.$this->arrange.'"';
-$ag_inlineParams.=' newImageTag="'.$this->newImageTag.'"';
-$ag_inlineParams.=' newImageDays="'.$this->newImageDays.'"';
-$ag_inlineParams.=' frameWidth="'.$this->frameWidth.'"';
-$ag_inlineParams.=' frameHeight="'.$this->frameHeight.'"';
-$ag_inlineParams.=' showSignature="'.$this->showSignature.'"';
-$ag_inlineParams.=' plainTextCaptions="'.$this->plainTextCaptions.'"';
-$ag_inlineParams.=' popupEngine="'.$this->popupEngine.'"';
-$ag_inlineParams.=' backgroundColor="'.$this->backgroundColor.'"';
-$ag_inlineParams.=' foregroundColor="'.$this->foregroundColor.'"';
-$ag_inlineParams.=' highlightColor="'.$this->highlightColor.'"';
+use Joomla\CMS\Factory;
+use Joomla\CMS\Plugin\PluginHelper;
 
-// Albums Support
-$ag_inlineParams.=' albumUse="'.$this->albumUse.'"';
-// Pagination Support
-$ag_inlineParams.=' paginUse="'.$this->paginUse.'"';
-$ag_inlineParams.=' paginImagesPerGallery="'.$this->paginImagesPerGallery.'"';
+PluginHelper::importPlugin( 'content' );
 
-JPluginHelper::importPlugin( 'content' );
-
-$article = new JObject();
-
-$app = JFactory::getApplication();
+$app = Factory::getApplication();
 $active = $app->getMenu()->getActive();
+$params = $active->getParams();
 
-$article->text = null;
-//Display page heading
-if($active->params->get('show_page_heading'))
+$ag_params='';
+foreach($params as $key => $value)
 {
-    $article->text = '<h1>'.$active->params->get('page_title').'</h1>';
+    $ag_inlineParams.=' '.$key .'="'.$value.'"';
 }
-$article->text .= '{AG '.$ag_inlineParams.' }'.$this->galleryName.'{/AG}';
-$article->id = 0;
-$limitstart = 0;
-$dispatcher = JDispatcher::getInstance();
-$results = $dispatcher->trigger('onContentPrepare', array ( &$context, &$article, & $params, $limitstart));
-echo $article->text;
 
-?>
+$article = new \stdClass();
+
+//Display page heading
+if($active->getParams()->get('show_page_heading'))
+{
+    $article->text = '<h1>'.$active->getParams()->get('page_title').'</h1>';
+}
+$article->text .= '{AG '.$ag_params.' }'.$params->get('galleryName').'{/AG}';
+$article->id = $active->id;
+
+$app->triggerEvent('onContentPrepare', array ( &$context, &$article, & $params));
+echo $article->text;
--- com_admirorgallery/site/views/layout/view.html.php
+++ com_admirorgallery/site/views/layout/view.html.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @version     5.5.0
  * @package     Admiror Gallery (component)
@@ -16,35 +17,6 @@ class AdmirorgalleryViewLayout extends JViewLegacy
 
     function display($tpl = null)
     {
-
-        $mainframe = JFactory::getApplication('site');
-        $params = $mainframe->getParams();
-
-        $this->assign('galleryName', $params->get('galleryName'));
-        $this->assign('template', $params->get('template'));
-        $this->assign('thumbWidth', $params->get('thumbWidth'));
-        $this->assign('thumbHeight', $params->get('thumbHeight'));
-        $this->assign('thumbAutoSize', $params->get('thumbAutoSize'));
-        $this->assign('arrange', $params->get('arrange'));
-        $this->assign('newImageTag', $params->get('newImageTag'));
-        $this->assign('newImageDays', $params->get('newImageTag_days'));
-        $this->assign('frameWidth', $params->get('frame_width'));
-        $this->assign('frameHeight', $params->get('frame_height'));
-        $this->assign('showSignature', $params->get('showSignature'));
-        $this->assign('popupEngine', $params->get('popupEngine'));
-        $this->assign('foregroundColor', $params->get('foregroundColor'));
-        $this->assign('backgroundColor', $params->get('backgroundColor'));
-        $this->assign('highlightColor', $params->get('highlightColor'));
-        $this->assign('plainTextCaptions', $params->get('plainTextCaptions'));
-
-        // Albums Support
-        $this->assign('albumUse', $params->get('albumUse'));
-        // Pagination Support
-        $this->assign('paginUse', $params->get('paginUse'));
-        $this->assign('paginImagesPerGallery', $params->get('paginImagesPerGallery'));
-
-
         parent::display($tpl);
-
     }
 }
