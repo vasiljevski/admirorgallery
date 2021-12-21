659d22259124553edf606f9c5888377c30147c93
Nikola Vasiljevski <nikola83@gmail.com>
2021-12-21 13:11:54 +0100

    Fixing compatibility issues with J3

--- com_admirorgallery/admirorbutton/admirorbutton.php
+++ com_admirorgallery/admirorbutton/admirorbutton.php
@@ -48,8 +48,8 @@ class plgButtonAdmirorbutton extends CMSPlugin
     public function onDisplay($name, $asset, $author)
     {
         $app       = Factory::getApplication();
+        $user      = Factory::getUser();
         $doc       = $app->getDocument();
-        $user      = $app->getIdentity();
         $extension = $app->input->get('option');
 
         // For categories we check the extension (ex: component.section)
@@ -82,7 +82,7 @@ class plgButtonAdmirorbutton extends CMSPlugin
             $button->modal   = true;
             $button->link    = $link;
             $button->text    = Text::_('COM_ADMIRORGALLERY');
-            $button->name    = $this->_type . '_' . $this->_name;
+            $button->name    = 'pictures';
             $button->icon    = 'pictures';
             $button->iconSVG = '<svg width="24" height="24" viewBox="0 0 512 512"><path d="M464 64H48C21.49 64 0 85.49 0 112v288c0 26.51 21.49 48'
                 . ' 48 48h416c26.51 0 48-21.49 48-48V112c0-26.51-21.49-48-48-48zm-6 336H54a6 6 0 0 1-6-6V118a6 6 0 0 1 6-6h404a6 6'
@@ -90,8 +90,8 @@ class plgButtonAdmirorbutton extends CMSPlugin
                 . 'zM96 352h320v-80l-87.515-87.515c-4.686-4.686-12.284-4.686-16.971 0L192 304l-39.515-39.515c-4.686-4.686-12.284-4'
                 . '.686-16.971 0L96 304v48z"></path></svg>';
             $button->options = [
-                'height'          => '300px',
-                'width'           => '400px',
+                'height'          => '800px',
+                'width'           => '500px',
                 'bodyHeight'      => '70',
                 'modalWidth'      => '80',
                 'tinyPath'        => $link,
