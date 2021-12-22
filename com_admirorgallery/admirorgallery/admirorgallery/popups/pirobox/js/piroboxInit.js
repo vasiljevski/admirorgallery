8017b54f7cf9ab980230a06474cf98c5664d1e29
Nikola Vasiljevski <nikola83@gmail.com>
2021-12-22 18:00:26 +0100

    Minor updates

--- com_admirorgallery/admirorgallery/admirorgallery/popups/pirobox/js/piroboxInit.js
+++ com_admirorgallery/admirorgallery/admirorgallery/popups/pirobox/js/piroboxInit.js
@@ -3,7 +3,7 @@
  * @package     Admiror Gallery (plugin)
  * @subpackage  admirorgallery
  * @author      Igor Kekeljevic & Nikola Vasiljevski
- * @copyright   Copyright (C) 2010 - 2020 http://www.admiror-design-studio.com All Rights Reserved.
+ * @copyright   Copyright (C) 2010 - 2021 http://www.admiror-design-studio.com All Rights Reserved.
  * @license     http://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
  */
 
