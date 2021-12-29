1df5a3c119759dad96a8ea53d27495f6241c4628
Nikola Vasiljevski <Nikola.Vasiljevski@partner.bmw.de>
2021-12-29 13:39:54 +0100

    Updated header tag order

--- com_admirorgallery/admirorgallery/admirorgallery/core/agErrorHandler.php
+++ com_admirorgallery/admirorgallery/admirorgallery/core/agErrorHandler.php
@@ -5,8 +5,8 @@
  * @subpackage  Content.AdmirorGallery
  * @author      Igor Kekeljevic & Nikola Vasiljevski
  * @copyright   Copyright (C) 2010 - 2021 https://www.admiror-design-studio.com All Rights Reserved.
- * @since       5.5.0
- * @license     https://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+*  @license     https://www.gnu.org/licenses/gpl-2.0.html GNU/GPL
+*  @since       5.5.0
  */
 
 namespace Admiror\Plugin\Content\AdmirorGallery;
